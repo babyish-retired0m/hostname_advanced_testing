381d924c6d1f7d0c3d946ae6d02b82d92883181c
John Doe <97737897+babyish-retired0m@users.noreply.github.com>
2022-05-19 19:39:24 +0300

    Add files via upload

--- results.py
+++ results.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = "1.1"
+__version__ = "1.3"
 import time
 import json
 import os
@@ -25,13 +25,18 @@ def __get_show_result_verbose_ping__(verbose_ping, host):
 		print("\n")
 	else: print("{0}{3} {2}{4:11} {1}completed: true{2}".format(utility.Clr.YELLOW,utility.Clr.GREEN,utility.Clr.RST,host,"ping"))
 def __get_show_result_verbose_traceroute__(verbose_traceroute, host):
-	if verbose_traceroute["host_reached"]: print("{0}{3} {2}{4:11} {1}completed: true{2}".format(utility.Clr.YELLOW,utility.Clr.GREEN,utility.Clr.RST,host,"traceroute"))
-	else:
+	def __get_show_traceroute__(verbose_traceroute):
 		for (enum,record) in enumerate(verbose_traceroute["traceroute"],start=1):
 			if enum<10: text=" "
 			else:text=""
 			if record is None: print(f"{enum}{text}  {utility.Clr.RED}* * *{utility.Clr.RST}")
 			else: print(f"{enum}{text}  {record[0]} ({record[1]})  {record[2]}")
+	if verbose_traceroute["host_reached"]:
+		print("host: {0}{3} {2}{4:11} {1}reached: true{2}".format(utility.Clr.YELLOW,utility.Clr.GREEN,utility.Clr.RST,host,"traceroute"))
+		if None in verbose_traceroute["traceroute"]:
+			__get_show_traceroute__(verbose_traceroute)
+	else:
+		__get_show_traceroute__(verbose_traceroute)
 def __get_show_result_resolve__(resolve):
 		for record in resolve:
 			if "A" in resolve[record]:
@@ -49,19 +54,27 @@ def get_show_results(results):
 			__get_show_result_verbose_traceroute__(hosts[host]["verbose_traceroute"], host)
 			
 if __name__ == '__main__':
-	contents_results3=file.get_request_text_as_json("https://api.github.com/repos/babyish-retired0m/hostname_advanced_testing/contents/results3?ref=main")
-	if "message" in contents_results3:
-		if contents_results3["message"].startswith("API rate limit exceeded for"):
-			print("API rate limit exceeded, (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.), documentation_url https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")
-	else:
-		for (enum,result) in enumerate(contents_results3,start=0):
-			if result["name"] != ".gitkeep":
-				print("Num #:"+str(enum),result["name"])
-		enum_input = ""
-		while True:
-			enum_input = input("e -EXIT, Num #: 1 - "+str(len(contents_results3)-1)+str("\n")+str("Num #:"))
-			if enum_input=="e" or int(enum_input)==0 or int(enum_input)>=len(contents_results3): break
-			else: 
-				get_contents=(contents_results3[int(enum_input)]["download_url"])
-				get_show_results(file.get_request_text_as_json(get_contents))
-				break
+	"""dir_parent =  os.path.dirname(__file__);dir_results = "/results/";file_result = "results_2022-05-18_16-35-48_hosts_all.json"
+	path_json = dir_parent + dir_results + file_result
+	results=json.load(open(path_json))
+	get_show_results(results)"""
+	#results=(file.get_request_text_as_json("https://api.github.com/repos/babyish-retired0m/hostname_advanced_testing/contents/results3?ref=main"))
+	
+	def github_repos():
+		contents_results3=file.get_request_text_as_json("https://api.github.com/repos/babyish-retired0m/hostname_advanced_testing/contents/results3?ref=main")
+		if "message" in contents_results3:
+			if contents_results3["message"].startswith("API rate limit exceeded for"):
+				print("API rate limit exceeded, (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.), documentation_url https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")
+		else:
+			for (enum,result) in enumerate(contents_results3,start=0):
+				if result["name"] != ".gitkeep":
+					print("Num #:"+str(enum),result["name"])
+			enum_input = ""
+			while True:
+				enum_input = input("e -EXIT, Num #: 1 - "+str(len(contents_results3)-1)+str("\n")+str("Num #:"))
+				if enum_input=="e" or int(enum_input)==0 or int(enum_input)>=len(contents_results3): break
+				else: 
+					get_contents=(contents_results3[int(enum_input)]["download_url"])
+					get_show_results(file.get_request_text_as_json(get_contents))
+					break
+	github_repos()
