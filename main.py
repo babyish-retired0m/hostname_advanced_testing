1c2e1196269db9ef7c8bd74ba60593422da83888
John Doe <97737897+babyish-retired0m@users.noreply.github.com>
2022-12-09 07:23:57 +0200

    Add files via upload
    __version__ = "3.1"
    """
    self.can_not_be_ssl_check_path
    write_text_as_json self.can_not_be_ssl_check
    self.can_not_be_resolve
    write_text_as_json self.can_not_be_resolve
    """

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-__version__ = "2.4"
+__version__ = "3.1"
+"""
+self.can_not_be_ssl_check_path
+write_text_as_json self.can_not_be_ssl_check
+self.can_not_be_resolve
+write_text_as_json self.can_not_be_resolve
+"""
 import utilities.dns_resolve as dns_resolve
 import utilities.ping as ping
 import utilities.traceroute as traceroute
-try:
-	import icmplib
-except ImportError:
-	raise SystemExit("Please install icmplib, pip3 install icmplib, (https://github.com/ValentinBELYN/icmplib)")
 import utilities.ssl_check as ssl_check
 import utilities.ip_address as ip_address
 import utilities.utility as utility
@@ -17,10 +19,13 @@ import json
 import sys
 import pathlib
 
+#utility.get_check_package('icmplib')
+import icmplib
+
 File = file.Main(print_result = False)
 
 class Advanced_testing:
-	def __init__(self, get_nslookup = True, get_ping = True, get_traceroute = True, get_ssl_check = True, get_dump = True, records = ["A", "AAAA", "CNAME", "MX", "SOA", "TXT", "NS"], nameserver = None, hostnames_dict = {}, path_results_name = None):
+	def __init__(self, get_nslookup = True, get_ping = True, get_traceroute = True, get_ssl_check = True, get_dump = True, records = ["A", "AAAA", "CNAME", "MX", "SOA", "TXT", "NS"], nameserver = None, hostnames_dict = {}, path_results_name = None, print_result = True, say_result = False):
 		self.get_nslookup = get_nslookup
 		self.get_ping = get_ping
 		self.get_traceroute = get_traceroute
@@ -31,125 +36,202 @@ class Advanced_testing:
 		if len(hostnames_dict) > 0:
 			self.recv_records = hostnames_dict
 		else:
-			#self.recv_records = {"parameters" : {"Unix Epoch Time" : utility.get_unix_time(), "Public IP Address" : self.__get_ip__()}}
-			self.recv_records = {"parameters" : {"Unix Epoch Time" : utility.get_unix_time()}}
+			self.recv_records = {"parameters" : {"Unix Epoch Time" : utility.get_time_unix(), 'timezone':'UTC'}}
+
 		self.path_results_name = path_results_name
 		self.parent_dir = os.path.dirname(__file__)
 		path = self.parent_dir + "/results/"
 		if File.check_dir(path) is False: File.dirs_make(path)
-		self.path_results = path + "results_" + time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime()) + ".json"
+		self.path_results = path + "results_" + time.strftime("%Y-%m-%d_%H-%M-%S", utility.get_time_local()) + ".json"
 		if self.path_results_name: self.path_results = self.path_results.replace(".json", "_hosts_" + self.path_results_name + ".json")
 
-		self.cannot_be_resolved = File.open_as_list(self.parent_dir + "/utilities/cannot_be_resolved.txt")
-		self.cannot_be_ssl_checked = File.open_as_list(self.parent_dir + "/utilities/cannot_be_ssl_checked.txt")
+		self.ip_address_resolved = None
+		self.can_not_be_resolve_path = self.parent_dir + "/utilities/can_not_be_resolve.txt"
+		self.can_not_be_resolve = File.open_as_list(self.can_not_be_resolve_path)
+		self.can_not_be_ssl_check_path = self.parent_dir + "/utilities/can_not_be_ssl_check.txt"
+		self.can_not_be_ssl_check = File.open_as_list(self.can_not_be_ssl_check_path)
+
+		self.print_result = print_result
+		self.say_result = say_result
 	"""def __call__(self):
 		return self.__get_hostname_advanced_testing__()"""
+
+
 	def __get_ip_public__(self):
-		self.ip_address_public = ip_address.get_ip_address_public_amazon()
-		if self.ip_address_public: self.__get_ip_address_checked__()
-	def __get_ip_address_checked__(self):
-		if ip_address.check_ip_in_network_lanet_ua(self.ip_address_public): self.ip_address_checked = "176.36.0.0/14"
-		else: self.ip_address_checked = self.ip_address_public
+		def __get_ip_address_check__():
+			self.ip_address_checked = "176.36.0.0/14" if ip_address.check_ip_in_network_lanet_ua(self.ip_address_public) else self.ip_address_public
+		self.ip_address_public = ip_address.get_ip_address_public_amazon(timeout_count = 100, timeout_sleep = 10)
+		__get_ip_address_check__()
+
+
 	def __get_nslookup__(self, qname):
 		Response = dns_resolve.Dns_response(host = qname, records = self.records, nameserver = self.nameserver, ip_address_public_answer = self.ip_address_public)
 		result = Response.get_nslookup()
 		recv_records = {"resolve":{"nslookup":result[qname]}}
-		recv_records["resolve"]["parameters"] = {"Unix Epoch Time":utility.get_unix_time(), "Public IP Address":self.ip_address_checked, "pcname":utility.getpcname(), "username":utility.getusername(), "currentdirectory":utility.getcurrentdirectory()}
+		recv_records["resolve"]["parameters"] = {"Unix Epoch Time":utility.get_time_unix(), "Public IP Address":self.ip_address_checked, "pcname":utility.get_pcname(), "username":utility.get_username(), "currentdirectory":utility.get_currentdirectory()}
 		return recv_records["resolve"]
+
+
 	def __get_ping__(self, qname):
 		result = ping.verbose_ping(address = qname, ip_address_resolved = self.ip_address_resolved)
 		if result is not None:
 			recv_records = {"verbose_ping":result[qname]}
-			recv_records["verbose_ping"]["parameters"]["Unix Epoch Time"] = utility.get_unix_time()
+			recv_records["verbose_ping"]["parameters"]["Unix Epoch Time"] = utility.get_time_unix()
 			recv_records["verbose_ping"]["parameters"]["Public IP Address"] = self.ip_address_checked
 			return recv_records["verbose_ping"]
+
+
 	def __get_traceroute__(self, qname):
 		result = traceroute.verbose_traceroute(address = qname, ip_address_resolved = self.ip_address_resolved)
-		recv_records = {"verbose_traceroute":result[qname]}
-		recv_records["verbose_traceroute"]["parameters"]["Unix Epoch Time"] = utility.get_unix_time()
-		recv_records["verbose_traceroute"]["parameters"]["Public IP Address"] = self.ip_address_checked
-		return recv_records["verbose_traceroute"]
+		if result is not None:
+			recv_records = {"verbose_traceroute":result[qname]}
+			recv_records["verbose_traceroute"]["parameters"]["Unix Epoch Time"] = utility.get_time_unix()
+			recv_records["verbose_traceroute"]["parameters"]["Public IP Address"] = self.ip_address_checked
+			return recv_records["verbose_traceroute"]
+
+
 	def __get_ssl_check__(self, qname):
-		if qname not in self.cannot_be_ssl_checked:
-			sslobject = ssl_check.SSLCheck()
-			try:
-				result = sslobject.show_result([qname])
-				if result is not None:
-					recv_records = result[qname]
-					return recv_records
-				else: self.cannot_be_ssl_checked.append(qname)
-			except: print("__get_ssl_check__ cannot be ssl checked")
-		else: pass
+		if ip_address.get_ip_address_valid(qname): pass
+		else:
+			if qname not in self.can_not_be_ssl_check:
+				sslobject = ssl_check.SSLCheck()
+				try:
+					result = sslobject.show_result([qname])
+					if result is not None:
+						recv_records = result[qname]
+						return recv_records
+					else:
+						self.can_not_be_ssl_check.append(qname)
+						File.write_text_as_json(self.can_not_be_ssl_check_path, self.can_not_be_ssl_check)
+				except: print("__get_ssl_check__ cannot be ssl checked")
+			else: pass
+
+
+	def __get_ip_address_resolved__(self, qname):
+		try:
+			if ip_address.get_ip_address_valid(qname):
+				self.ip_address_resolved = qname
+				self.ip_address_flag = True
+			else:
+				self.ip_address_resolved = icmplib.resolve(qname)[0]
+			return True
+		except:
+			print(utility.warp_red('The name ' + qname + ' can not be resolved'))
+			self.can_not_be_resolve.append(qname)
+			File.write_text_as_json(self.can_not_be_resolve_path, self.can_not_be_resolve)
+			return False
+
+
 	def get_hostname_advanced_testing(self, hostnames = "amazon.com"):
 		self.hostnames = hostnames
-		start_time = time.time()
+		self.start_time = time.time()
 		self.recv_records["advanced_test"] = {}
 		if isinstance(self.hostnames, str): self.hostnames = [self.hostnames]
 		elif isinstance(self.hostnames, list): pass
 		else: print("Are list of hostname(s) str, list?"); sys.exit(1)
-		hostnames_len = len(self.hostnames)
-		if hostnames_len > 1000:
-			if hostnames_len % 2 == 0: k = 100
-			else: k = 50
+		self.hostnames_len = len(self.hostnames)
+		if self.hostnames_len > 1000:
+			if self.hostnames_len % 2 == 0: self.k = 100
+			else: self.k = 50
 		else:
-			if hostnames_len % 2 == 0: k = 200
-			else: k = 150
+			if self.hostnames_len % 2 == 0: self.k = 200
+			else: self.k = 150
 		for qname in self.hostnames:
+			start_time_advanced_test = time.time()
 			self.__get_ip_public__()
-			if len(qname)>3:
-				if qname in self.cannot_be_resolved: pass
-				else:
-					try: 
-						self.ip_address_resolved = icmplib.resolve(qname)[0]
-					except:
-						print(f"The name '{qname}' cannot be resolved")
-						self.cannot_be_resolved.append(qname)
-						pass
-					else:
-						self.recv_records["advanced_test"][qname] = {}
-						utility.percents_print(self.hostnames.index(qname), hostnames_len)
-						#nslookup
-						if self.get_nslookup: self.recv_records["advanced_test"][qname]["resolve"] = self.__get_nslookup__(qname)
-						#ping
-						if self.get_ping: self.recv_records["advanced_test"][qname]["verbose_ping"] = self.__get_ping__(qname)
-						#traceroute
-						if self.get_traceroute: self.recv_records["advanced_test"][qname]["verbose_traceroute"] = self.__get_traceroute__(qname)
-						#ssl_check
-						if self.get_ssl_check: self.recv_records["advanced_test"][qname]["ssl_cert_info"] = self.__get_ssl_check__(qname)
+
+			if len(qname)>3 and qname not in self.can_not_be_resolve and self.__get_ip_address_resolved__(qname):
+
+				self.recv_records["advanced_test"][qname] = {}
+				print('\n\n')
+				utility.print_yellow2('advanced_test start ' + time.strftime("%H:%M:%S", time.gmtime(start_time_advanced_test)))
+				utility.print_percents(self.hostnames.index(qname), self.hostnames_len)
+
+				#nslookup
+				if self.get_nslookup:
+					print('\n\n')
+					utility.print_yellow2('NS LOOK UP testing ' + qname)
+					self.recv_records["advanced_test"][qname]["resolve"] = self.__get_nslookup__(qname)
+
+				#ping
+				if self.get_ping:
+					print('\n\n')
+					utility.print_yellow2('Ping testing' + qname)
+					self.recv_records["advanced_test"][qname]["verbose_ping"] = self.__get_ping__(qname)
+
+				#traceroute
+				if self.get_traceroute:
+					print('\n\n')
+					utility.print_yellow2('Traceroute to ' + qname + ' testing')
+					self.recv_records["advanced_test"][qname]["verbose_traceroute"] = self.__get_traceroute__(qname)
+
+				#ssl_check
+				if self.get_ssl_check:
+					print('\n\n')
+					utility.print_yellow2('SSL check testing')
+					self.recv_records["advanced_test"][qname]["ssl_cert_info"] = self.__get_ssl_check__(qname)
+
 			if self.hostnames.index(qname) > 0:
-				if self.hostnames.index(qname) % k == 0:
-					duration_seconds = time.time() - start_time
-					duration = time.strftime("%H:%M:%S", time.gmtime(duration_seconds))
-					print("List " + str(self.hostnames.index(qname)) + " of", str(hostnames_len), "duration:", duration)
-					if self.get_dump: self.__get_dump__()
-				else:
-					duration = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
-					self.border_msg(utility.Clr.YELLOW2 + 'List ' + str(self.hostnames.index(qname)) + ' of ' + str(hostnames_len) + ' duration: ' + duration + utility.Clr.RST2 + time.ctime())
-		duration_seconds = time.time() - start_time
-		duration = time.strftime("%H:%M:%S", time.gmtime(duration_seconds))
-		self.recv_records["parameters"]["Execution time Duration"] = str('{:.3f}'.format(duration_seconds))
-		print("Duration:", duration)
-		os.system('say ' + "Dns hostnames advanced tests jobs done, time taken:: " + duration)
-		if self.get_dump: self.__get_dump__()
+				if self.hostnames.index(qname) % self.k == 0: self.__get_dump__()
+				self.__print_result_duration__(qname)
+		self.__print_result_execution_time_Duration__()
+		self.__get_dump__()
+
 		return self.recv_records
+
+
 	def __get_dump__(self):
-		json.dump(self.recv_records, fp = open(self.path_results, 'w'), indent=4)
-		print(utility.Clr.GREEN2 + "Results dumped:", self.path_results + utility.Clr.RST2)
-		os.system('say ' + "Results dumped")
+		if self.get_dump:
+			json.dump(self.recv_records, fp = open(self.path_results, 'w'), indent=4)
+			if self.print_result: utility.print_green2("Results dumped:" + self.path_results)
+			if self.say_result: utility.get_say('Results dumped')
+
+
+	def __print_result_duration__(self, qname):
+		if self.print_result:
+			duration_seconds = time.time() - self.start_time
+			duration = time.strftime("%H:%M:%S", time.gmtime(duration_seconds))
+			self.border_msg(utility.Clr.YELLOW2 + 'List ' + str(self.hostnames.index(qname)) + ' of ' + str(self.hostnames_len) + ' duration: ' + duration + utility.Clr.RST2 + ' ' + time.ctime())
+
+
+	def __print_result_execution_time_Duration__(self, say_result = False):
+		if self.print_result:
+			duration_seconds = time.time() - self.start_time
+			duration = time.strftime("%H:%M:%S", time.gmtime(duration_seconds))
+			self.recv_records["parameters"]["Execution time Duration"] = str('{:.3f}'.format(duration_seconds))
+			print("Duration:", duration)
+		if self.say_result: utility.get_say("Dns hostnames advanced tests jobs done, time taken: " + duration)
+
 
 	def border_msg(self, message):
 		"""Print the message in the box."""
+		#row = len(ord(message))
 		row = len(message)
 		#print('row = len(message)',row)
 		row_len = 60
-		row = row if row < row_len else row_len
+		row = row_len if row < row_len else row
 		row_len_result = int(row / 4)
 		#print('row',row,'row_len_result',row_len_result)
 		i = ''.join([(row_len_result - 2) * ' '])
 		h = ''.join(['+'] + ['-' * row] + ['+'])
-		result = h + '\n' + "|" + i + message + i + "|"'\n' + h
+		result = h + '\n' + '|' + i + message + i + '|' + '\n' + h
 		print(result)
 
+
 if __name__ == '__main__':
-	Get_hostnames_testing = Advanced_testing(get_nslookup = True, get_ping = False, get_traceroute = False, get_ssl_check = False, get_dump = False, records = ["A", "CNAME"], nameserver = "1.1.1.1")
-	print(Get_hostnames_testing.get_hostname_advanced_testing(hostnames = ["www.facebook.com", "amazon.com", "us2723.nordvpn.com", "jp590.nordvpn.com", "za110.nordvpn.com"]))
+	#Get_hostnames_testing = Advanced_testing(get_nslookup = True, get_ping = False, get_traceroute = False, get_ssl_check = False, get_dump = False, records = ["A", "CNAME"], nameserver = '1.1.1.1')
+
+	Get_hostnames_testing = Advanced_testing(get_nslookup = True, get_ping = True, get_traceroute = True, get_ssl_check = False, get_dump = False, records = ["A", "CNAME"], nameserver = ['1.1.1.1', '8.8.4.4'])
+	try:
+		print(Get_hostnames_testing.get_hostname_advanced_testing(hostnames = ['www.facebook.com',
+		'amazon.com',
+		 'us2723.nordvpn.com',
+		  'jp590.nordvpn.com',
+		   'za110.nordvpn.com',
+		   'fbi.gov',
+		   'www.ic3.gov',
+		   'ic3.gov'
+		   ]))
+	except Exception as e:
+		# raise e
+		print('KeyboardInterrupt')
--- results.py
+++ results.py
@@ -5,27 +5,47 @@ import json
 import os
 import utilities.utility as utility
 import utilities.file as file
+
 File = file.Main(print_result=False)
 start_time = time.time()
+
 def __get_show_result_parameters__(parameters):
-	print('{1}Advanced test parameters{6}:\nPublic IP Address: {4}{7}{0} Time: {5}{8}{0}, Execution time: {5}{9}{0}'.format(utility.Clr.RST, utility.Clr.RED2, utility.Clr.GREEN, utility.Clr.YELLOW, utility.Clr.VIOLET, utility.Clr.PINK, utility.Clr.RST2, parameters["Public IP Address"], time.ctime(parameters["Unix Epoch Time"]),parameters["Execution time Duration"]))
+	utility.print_violet2('Advanced test parameters')
+	utility.print_yellow('Time: ' + time.ctime(parameters['Unix Epoch Time']) + ' timezone: ' + parameters.get('timezone', 'UTC'))
+
+	#print('{1}Advanced test parameters{6}:\nPublic IP Address: {4}{7}{0} Time: {5}{8}{0}, Execution time: {5}{9}{0}'.format(utility.Clr.RST, utility.Clr.RED2, utility.Clr.GREEN, utility.Clr.YELLOW, utility.Clr.VIOLET, utility.Clr.PINK, utility.Clr.RST2, parameters["Public IP Address"], time.ctime(parameters["Unix Epoch Time"]),parameters["Execution time Duration"]))
+
+
 def __get_show_result_parameters_resolve__(parameters, host):
-	print('\n\n\n{1}{10:15}{9} host: {3}{4}{0} \nPublic IP Address: {7}{5}{0} Time: {8}{6}{0}'.format(utility.Clr.RST, utility.Clr.GREY2, utility.Clr.GREEN, utility.Clr.YELLOW, host, parameters["Public IP Address"], time.ctime(parameters["Unix Epoch Time"]), utility.Clr.VIOLET, utility.Clr.PINK, utility.Clr.RST2, "Resolve test"))
+	print('\n\n\n')
+	utility.print_grey('Resolve test host:')
+	utility.print_yellow(host)
+	print('Public IP Address: ', end='')
+	#utility.print_violet(parameters)
+	utility.print_violet(parameters['parameters']['Public IP Address'])
+	print('Time:', time.ctime(parameters['parameters']['Unix Epoch Time']))
+
+
 def __get_show_result_verbose_ping__(verbose_ping, host):
 	parameters=verbose_ping["parameters"]
 	statistics=verbose_ping["statistics"]
 	if statistics["packet_loss"]>0:
-		print("{5}{4:15}{6} host: {0}{3:11} {2}{1}completed: False{6}".format(utility.Clr.YELLOW, utility.Clr.RED2, utility.Clr.RST, host, "Ping test", utility.Clr.GREY2, utility.Clr.RST2))
+		print(utility.warp_grey('Ping test'),'host:', utility.warp_yellow(host), utility.warp_red('completed: False'))
 		print(f"{parameters.get('username','anonymous')}@{parameters.get('pcname','2ip')} {parameters.get('currentdirectory','~')} $ ",end='')
 		print(f"ping -c {parameters['count']} {parameters['address']}")
 		print(f"PING {parameters['address']}({parameters['ip_address']}): {parameters.get('payload_size','56')} data bytes")
 		for record in verbose_ping["ping"]:
-			if record is None: print("{1}Request timeout for icmp_seq{0}".format(utility.Clr.RST,utility.Clr.RED))
+			if record is None: utility.print_red('Request timeout for icmp_seq')
 			else: print(f"{record[0]} bytes from {record[1]}: icmp_seq={record[2]} time={record[3]}")
-		print("--- {1}{2}{0} ping statistics ---".format(utility.Clr.RST,utility.Clr.YELLOW,parameters['address']))
-		print("{3} packets transmitted, {1}{4} packets received{2}, {0}{5:.1f}% packet loss{2}".format(utility.Clr.RED,utility.Clr.GREEN,utility.Clr.RST,statistics['packets_transmitted'],statistics['packets_received'],statistics['packet_loss']))
+		print('---', utility.warp_yellow(parameters['address']),'ping statistics ---')
+		
+		print(statistics['packets_transmitted'], 'packets transmitted', utility.warp_green(str(statistics['packets_received']) + ' packets received'), utility.warp_red(str(statistics['packet_loss']) + '% packet loss'))
+		# print(statistics['packets_transmitted'], 'packets transmitted', utility.warp_green(str(statistics['packets_received']) + ' packets received'), utility.warp_red('statistics['packet_loss']'), '% packet loss'.format(statistics['packet_loss']))
+		#print("{3} packets transmitted, {1}{4} packets received{2}, {0}{5:.1f}{2}".format(utility.Clr.RED,utility.Clr.GREEN,utility.Clr.RST,,,))
 		print("\n")
-	else: print("{5}{4:15}{6} host: {0}{3:11} {2}{1}completed: True{2}".format(utility.Clr.YELLOW, utility.Clr.GREEN, utility.Clr.RST, host, "Ping test", utility.Clr.GREY2, utility.Clr.RST2))
+	else: 
+		print(utility.warp_grey('Ping test'), 'host:', utility.warp_yellow(host), utility.warp_green('completed: True'))
+
 def __get_show_result_verbose_traceroute__(verbose_traceroute, host):
 	def __get_show_traceroute__(verbose_traceroute):
 		print("traceroute to "+ verbose_traceroute["parameters"]["address"],"("+verbose_traceroute["parameters"]["ip_address"]+")",verbose_traceroute["parameters"]["max_hops"],"hops max",verbose_traceroute["parameters"]["payload_size"],"byte packets")
@@ -34,15 +54,21 @@ def __get_show_result_verbose_traceroute__(verbose_traceroute, host):
 			else:text=""
 			if record is None: print(f"{enum}{text}  {utility.Clr.RED}* * *{utility.Clr.RST}")
 			else: print(f"{enum}{text}  {record[0]} ({record[1]})  {record[2]}")
-	if verbose_traceroute["host_reached"]:
-		print("{3}Traceroute test{4} host: {0}{5:11}{2} {1}reached: {6}{2}".format(utility.Clr.YELLOW, utility.Clr.GREEN, utility.Clr.RST, utility.Clr.GREY2, utility.Clr.RST2, host, verbose_traceroute["host_reached"]))
+	# host_reached = verbose_traceroute["host_reached"] if "host_reached" in verbose_traceroute else verbose_traceroute['statistics']["host_reached"]
+	host_reached = verbose_traceroute.get('host_reached') or verbose_traceroute['statistics'].get('host_reached', None)
+	# if verbose_traceroute["host_reached"]:
+	if host_reached:
+		print(utility.warp_grey('Traceroute test'), 'host:', utility.warp_yellow(host), utility.warp_green('host reached'))
 	else:
-		print("{7}Traceroute test{4} host: {0}{5:11}{2} {3}reached: {6}{4}".format(utility.Clr.YELLOW, utility.Clr.GREEN, utility.Clr.RST, utility.Clr.RED2, utility.Clr.RST2, host, verbose_traceroute["host_reached"], utility.Clr.GREY2))
+		print(utility.warp_grey('Traceroute test'), 'host:', utility.warp_yellow(host), utility.warp_red('host reached'))
 	if None in verbose_traceroute["traceroute"]:
-		print("{6}Traceroute test{4} host: {0}{5:11}{2} {3}luggage: False{4}".format(utility.Clr.YELLOW, utility.Clr.GREEN, utility.Clr.RST, utility.Clr.RED2, utility.Clr.RST2, host, utility.Clr.GREY2))
-		print(utility.Clr.GREY+"https://www.freebsd.org/cgi/man.cgi?query=traceroute\n- \"TRACEROUTE System Manager's Manual:\n * * *\nA more interesting example is:\n% traceroute allspice.lcs.mit.edu.\n... only knows what's going on with * * *.\""+utility.Clr.RST)
+		print(utility.warp_grey('Traceroute test'), 'host:', utility.warp_red2('luggage: False'))
+
+		print(utility.warp_grey("https://www.freebsd.org/cgi/man.cgi?query=traceroute\n- \"TRACEROUTE System Manager's Manual:\n * * *\nA more interesting example is:\n% traceroute allspice.lcs.mit.edu.\n... only knows what's going on with * * *.\""))
 		__get_show_traceroute__(verbose_traceroute)
-	elif verbose_traceroute["host_reached"] is False and not None in verbose_traceroute["traceroute"]: __get_show_traceroute__(verbose_traceroute)
+	elif host_reached is False and not None in verbose_traceroute["traceroute"]: __get_show_traceroute__(verbose_traceroute)
+
+
 def __get_show_result_resolve__(resolve):
 		for record in resolve:
 			if resolve["1.1.1.1"].get("A") != None:  check_dns = resolve["1.1.1.1"]["A"][0]
@@ -57,6 +83,8 @@ def __get_show_result_resolve__(resolve):
 				resolve[record]["nslookup"]["A"].sort()
 				if resolve[record]["nslookup"]["A"] != resolve[record]["nslookup"]["A"][0]: print("{0}Check  DNS record in NS {3} propagation {2}{1}".format(utility.Clr.RED, utility.Clr.RST, resolve[record]["nslookup"]["A"], record))
 				else: print("{0}Check DNS record {2}{1}".format(utility.Clr.RED,utility.Clr.RST, resolve[record]["nslookup"]["A"]))
+
+
 def get_show_results_hosts(results):
 	for result in results:
 		__get_show_result_parameters__(results[result]["parameters"])
@@ -67,20 +95,31 @@ def get_show_results_hosts(results):
 			__get_show_result_verbose_ping__(hosts[host]["verbose_ping"], host)
 			__get_show_result_verbose_traceroute__(hosts[host]["verbose_traceroute"], host)
 
+
 def get_show_results(results):
-	print(results["parameters"])
+	
 	#print(results["advanced_test"])
-	"""__get_show_result_parameters__(results["parameters"])
-	for host in results[:1]:
-		__get_show_result_parameters_resolve__(hosts[host]["resolve"]["parameters"], host)
-		__get_show_result_resolve__(hosts[host]["resolve"])
-		__get_show_result_verbose_ping__(hosts[host]["verbose_ping"], host)
-		__get_show_result_verbose_traceroute__(hosts[host]["verbose_traceroute"], host)"""
+	__get_show_result_parameters__(results["parameters"])
+	# for host in results[:1]:
+	#print(results['advanced_test']['dvprogram.state.gov']['resolve']['nslookup']['1.1.1.1']['A'])
+	#print(results['advanced_test']['dvprogram.state.gov']['resolve']['parameters'])
+	for host in results['advanced_test']:
+		# print(results[host])
+		# print(host)
+		# print(results['advanced_test'][host]['resolve'])
+		
+		__get_show_result_parameters_resolve__(results['advanced_test'][host]['resolve'], host)
+		#__get_show_result_resolve__(results['advanced_test'][host]['resolve'])
+		__get_show_result_verbose_ping__(results['advanced_test'][host]['verbose_ping'], host)
+		__get_show_result_verbose_traceroute__(results['advanced_test'][host]['verbose_traceroute'], host)
+
 
 if __name__ == '__main__':
-	dir_parent =  os.path.dirname(__file__);dir_results = "/results/";file_result = "results_2022-05-18_16-35-48_hosts_all.json"
-	path_json = dir_parent + dir_results + file_result
-	results=json.load(open(path_json))
+	# dir_parent =  os.path.dirname(__file__);
+	# dir_results = "/results/";
+	# file_result = "results_2022-05-18_16-35-48_hosts_all.json"
+	# path_json = dir_parent + dir_results + file_result
+	# results=json.load(open(path_json))
 	
 	import pathlib
 	
@@ -88,19 +127,24 @@ if __name__ == '__main__':
 	
 	def get_show_results_listing():
 		#listing_results = File.dir_listing_files_in_this_directory_tree(path="./results", file_extension="json")
-		listing_results = File.dir_listing_files_in_this_directory_tree(path= os.path.dirname(__file__) + "/results", file_extension="json")
+		listing_results = File.dir_listing_files_in_this_directory_tree(path = os.path.dirname(__file__) + "/results", file_extension="json")
 		#print(file_results)
-		for (enum,result) in enumerate(listing_results,start=0):
+		for (enum,result) in enumerate(listing_results, start=0):
 			#print(file_result.name)
-			print("Num #:"+str(enum),result.stem)
+			print("Number #:"+str(enum),result.stem)
 		enum_input = ""
 		while True:
-			enum_input = input("e -EXIT, Num #: 0 - " + str(len(listing_results)-1) + str("\n") + str("Num #:"))
-			if enum_input == "e" or int(enum_input)>=len(listing_results): break
+			# enum_input = input("Type e -for EXIT, Type Number (show result) #: 0 - " + str(len(listing_results)-1) + str("\n") + str("Number #:"))
+			enum_input = input('Type e -for EXIT, Type Number (show result) #: 0 - ' + str(len(listing_results)) + str('\n') + str('Number #:'))
+			if enum_input is 'e': break
+			elif int(enum_input) >= len(listing_results): break
 			else: 
 				get_contents=(listing_results[int(enum_input)])
-				print(File.open_json(get_contents))
-				break
+				#print(File.open_json(get_contents))
+				return (File.open_json(get_contents))
+				#break
+			#elif int(enum_input)<len(listing_results): break
+			
 	get_show_results(get_show_results_listing())
 	#list(p.glob('**/*.py'))
 	#get_show_results(results)
